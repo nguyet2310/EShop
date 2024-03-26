1586fec277a86ec199a15dba3146ae8e2a474080
nguyet2310 <phamnguyet2310@gmail.com>
2024-03-27 01:00:47 +0700

    create model and dbset

---
+++ EShop/Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Yêu cầu nhập Tên Sản phẩm")]
+        public string Name { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Yêu cầu nhập Mô tả Sản phẩm")]
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Yêu cầu nhập Giá Sản phẩm")]
+        public decimal Price { get; set; }
+        public int BrandId { get; set; }
+        public int CategoryId { get; set; }
+        public BrandModel Brand { get; set; }
+        public CategoryModel Category { get; set; }
+    }
+}

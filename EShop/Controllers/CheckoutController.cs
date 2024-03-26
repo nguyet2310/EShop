929313cce9043462d438debe92faa516786386f4
nguyet2310 <phamnguyet2310@gmail.com>
2024-03-26 23:18:34 +0700

    create view (index)

---
+++ EShop/Controllers/CheckoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EShop/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

7b2d138fc7d6cc8c0bb8434e53c9f06b4d6f2f3c
nguyet2310 <phamnguyet2310@gmail.com>
2024-03-28 00:44:04 +0700

    display products by category, brand

---
+++ EShop/Controllers/BrandController.cs
@@ -0,0 +1,29 @@
+﻿using EShop.Models;
+using EShop.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Controllers
+{
+    public class BrandController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public BrandController(DataContext context)
+        {
+            _dataContext = context;
+        }
+        public async Task<IActionResult> Index(string slug = "")
+        {
+            BrandModel brand = _dataContext.Brands.Where(c => c.Slug == slug).FirstOrDefault();
+            if (brand == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var productsByBrand = _dataContext.Products.Where(p => p.BrandId == brand.Id);
+
+            return View(await productsByBrand.OrderByDescending(p => p.Id).ToListAsync());
+        }
+    }
+}
--- EShop/Controllers/CategoryController.cs
+++ EShop/Controllers/CategoryController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EShop.Models;
+using EShop.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EShop.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly DataContext _dataContext;
+
+        public CategoryController(DataContext context) 
         {
-            return View();
+            _dataContext = context;
+        }
+        public async Task<IActionResult> Index(string slug="")
+        {
+            CategoryModel category = _dataContext.Categories.Where(c => c.Slug == slug).FirstOrDefault();
+            if(category == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var productsByCategory = _dataContext.Products.Where(p => p.CategoryId == category.Id);
+
+            return View(await productsByCategory.OrderByDescending(p => p.Id).ToListAsync());
         }
 
     }

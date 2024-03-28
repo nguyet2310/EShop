392b3be0d7fb553b2942f892d2c0122178080856
nguyet2310 <phamnguyet2310@gmail.com>
2024-03-29 01:25:08 +0700

    List Cart View

--- EShop/Controllers/CartController.cs
+++ EShop/Controllers/CartController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EShop.Models;
+using EShop.Models.ViewModels;
+using EShop.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EShop.Controllers
 {
     public class CartController : Controller
     {
+        private readonly DataContext _dataContext;
+
+        public CartController(DataContext dataContext)
+        {
+            _dataContext=dataContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<CartItemModel> cartItems = HttpContext.Session.GetJson<List<CartItemModel>>("Cart") ?? new List<CartItemModel>();
+            CartItemViewModel cartVM = new ()
+            {
+                CartItems = cartItems,
+                GrandTotal = cartItems.Sum(x => x.Quantity * x.Price)
+            };
+            return View(cartVM);
         }
         public IActionResult Checkout()
         {

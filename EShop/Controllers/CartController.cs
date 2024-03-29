2df7663829dc348a62b88bca4eb0d5c65d74c893
nguyet2310 <phamnguyet2310@gmail.com>
2024-03-30 03:03:09 +0700

    Add products to cart

--- EShop/Controllers/CartController.cs
+++ EShop/Controllers/CartController.cs
@@ -15,6 +15,7 @@ namespace EShop.Controllers
         }
         public IActionResult Index()
         {
+            // Lấy giỏ hàng từ session, nếu không có sẽ tạo mới
             List<CartItemModel> cartItems = HttpContext.Session.GetJson<List<CartItemModel>>("Cart") ?? new List<CartItemModel>();
             CartItemViewModel cartVM = new ()
             {
@@ -26,6 +27,32 @@ namespace EShop.Controllers
         public IActionResult Checkout()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Add(int id)
+        {
+            ProductModel product = await _dataContext.Products.FindAsync(id);
+
+            // Lấy giỏ hàng từ session với từ khóa "Cart" và chuyển đổi dữ liệu đó thành
+            // một đối tượng kiểu List<CartItemModel>, nếu không có sẽ tạo mới
+            List<CartItemModel> cart = HttpContext.Session.GetJson<List<CartItemModel>>("Cart") ?? new List<CartItemModel>();
+
+            // Tiến hành thêm sản phẩm vào giỏ hàng
+            CartItemModel cartItems = cart.Where(c => c.ProductId==id).FirstOrDefault();
+
+            if(cartItems==null)
+            {
+                cart.Add(new CartItemModel(product));
+            }
+            else
+            {
+                cartItems.Quantity += 1;
+            }
+
+            // Lưu lại danh sách giỏ hàng đã cập nhật vào session
+            HttpContext.Session.SetJson("Cart", cart);
+
+            return Redirect(Request.Headers["Referer"].ToString());
         }
     }
 }

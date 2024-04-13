ad67f430b2ef9811f73cc5414b35a611b535e885
nguyet2310 <phamnguyet2310@gmail.com>
2024-04-14 00:37:17 +0700

    Create OrderModel & OrderDetailsModel

---
+++ EShop/Models/OrderModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EShop.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public string OrderCode { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int Status { get; set; }
+    }
+}

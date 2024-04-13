b812b8b41c2c95f55b4e6065d6f84b90c556c120
nguyet2310 <phamnguyet2310@gmail.com>
2024-04-13 23:44:47 +0700

    Login user

--- EShop/Controllers/AccountController.cs
+++ EShop/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using EShop.Models;
+using EShop.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,10 +15,10 @@ namespace EShop.Controllers
 			_userManager = userManager;
 			_signInManager = signInManager;
 		}
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
         public IActionResult Create()
         {
@@ -30,11 +31,11 @@ namespace EShop.Controllers
 			if(ModelState.IsValid)
 			{
 				AppUserModel newUser = new AppUserModel{ UserName=user.Username, Email=user.Email };
-				IdentityResult result = await _userManager.CreateAsync(newUser);
+				IdentityResult result = await _userManager.CreateAsync(newUser, user.Password);
 				if (result.Succeeded)
 				{
 					TempData["success"] = "Tạo user thành công";
-					return Redirect("/account");
+					return Redirect("/account/login");
 				}
 				foreach(IdentityError error in result.Errors)
 				{
@@ -44,9 +45,24 @@ namespace EShop.Controllers
 			return View(user);
 		}
 
-		public async Task<IActionResult> Login()
+		public IActionResult Login(string returnUrl)
 		{
-			return View();
+			return View(new LoginViewModel { ReturnUrl= returnUrl });
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel loginVM)
+		{
+			if(ModelState.IsValid)
+			{
+				Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(loginVM.Username, loginVM.Password, false, false);
+				if (result.Succeeded)
+				{
+					return Redirect(loginVM.ReturnUrl ?? "/");
+				}
+				ModelState.AddModelError("", "username hoặc password bị sai!");
+			}
+			return View(loginVM);
 		}
 	}
 }

fca7611bef13e4784e29fbddcfc2dc3c5169dd71
nguyet2310 <phamnguyet2310@gmail.com>
2024-07-18 00:38:14 +0700

    List Users

---
+++ EShop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using EShop.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public UserController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Users.OrderByDescending(x=>x.Id).ToListAsync());
+        }
+    }
+}

4d1fce3cfaa9843a0a06726d3258215210ca979b
nguyet2310 <phamnguyet2310@gmail.com>
2024-07-26 23:26:02 +0700

    Create User

--- EShop/Areas/Admin/Controllers/UserController.cs
+++ EShop/Areas/Admin/Controllers/UserController.cs
@@ -1,6 +1,9 @@
-﻿using EShop.Repository;
+﻿using EShop.Models;
+using EShop.Repository;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace EShop.Areas.Admin.Controllers
@@ -9,15 +12,23 @@ namespace EShop.Areas.Admin.Controllers
     [Authorize]
     public class UserController : Controller
     {
-        private readonly DataContext _dataContext;
+        private readonly UserManager<AppUserModel> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UserController(DataContext dataContext)
+        public UserController(UserManager<AppUserModel> userManager, RoleManager<IdentityRole> roleManager)
         {
-            _dataContext = dataContext;
+            _userManager=userManager;
+            _roleManager=roleManager;
         }
         public async Task<IActionResult> Index()
         {
-            return View(await _dataContext.Users.OrderByDescending(x=>x.Id).ToListAsync());
+            return View(await _userManager.Users.OrderByDescending(x=>x.Id).ToListAsync());
+        }
+        public async Task<IActionResult> Create()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            ViewBag.Role = new SelectList(roles, "Id", "Name");
+            return View(new AppUserModel());
         }
     }
 }

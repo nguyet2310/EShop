5921d6fdd183430deae74dc49983290ee8d87391
nguyet2310 <phamnguyet2310@gmail.com>
2024-07-28 00:43:15 +0700

    Add Role

--- EShop/Areas/Admin/Controllers/RoleController.cs
+++ EShop/Areas/Admin/Controllers/RoleController.cs
@@ -1,5 +1,7 @@
-﻿using EShop.Repository;
+﻿using EShop.Models;
+using EShop.Repository;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,14 +11,33 @@ namespace EShop.Areas.Admin.Controllers
     public class RoleController : Controller
     {
         private readonly DataContext _dataContext;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public RoleController(DataContext dataContext)
+        public RoleController(DataContext dataContext, RoleManager<IdentityRole> roleManager)
         {
-            _dataContext=dataContext;
+            _dataContext = dataContext;
+            _roleManager = roleManager;
         }
         public async Task<IActionResult> Index()
         {
             return View(await _dataContext.Roles.OrderByDescending(r => r.Id).ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(IdentityRole model)
+        {
+            //avoid duplicate role
+            if(!_roleManager.RoleExistsAsync(model.Name).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(model.Name)).GetAwaiter().GetResult();
+            }
+            return Redirect("Index");
         }
     }
 }

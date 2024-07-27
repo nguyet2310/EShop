39b72b0b6ae3e2882362e78a398c37e34f77b298
nguyet2310 <phamnguyet2310@gmail.com>
2024-07-27 23:55:07 +0700

    Assign role for user

--- EShop/Areas/Admin/Controllers/UserController.cs
+++ EShop/Areas/Admin/Controllers/UserController.cs
@@ -12,17 +12,24 @@ namespace EShop.Areas.Admin.Controllers
     [Authorize]
     public class UserController : Controller
     {
+        private readonly DataContext _dataContext;
         private readonly UserManager<AppUserModel> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UserController(UserManager<AppUserModel> userManager, RoleManager<IdentityRole> roleManager)
+        public UserController(DataContext dataContext ,UserManager<AppUserModel> userManager, RoleManager<IdentityRole> roleManager)
         {
+            _dataContext=dataContext;
             _userManager=userManager;
             _roleManager=roleManager;
         }
         public async Task<IActionResult> Index()
         {
-            return View(await _userManager.Users.OrderByDescending(x=>x.Id).ToListAsync());
+            var usersWithRoles = await (from u in _dataContext.Users
+                                        join ur in _dataContext.UserRoles on u.Id equals ur.UserId
+                                        join r in _dataContext.Roles on ur.RoleId equals r.Id
+                                        select new { User = u, RoleName = r.Name }).ToListAsync();
+            return View(usersWithRoles);
+            //return View(await _userManager.Users.OrderByDescending(x => x.Id).ToListAsync());
         }
         public async Task<IActionResult> Create()
         {
@@ -35,19 +42,25 @@ namespace EShop.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(AppUserModel user)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 var createdUserResult = await _userManager.CreateAsync(user, user.PasswordHash);
-                if(createdUserResult.Succeeded)
+                if (createdUserResult.Succeeded)
                 {
+                    var createdUser = await _userManager.FindByEmailAsync(user.Email);
+                    var userId = createdUser.Id;
+                    var role = _roleManager.FindByIdAsync(user.RoleId); // lấy roleId
+                    //gán quyền
+                    var addToRoleResult = await _userManager.AddToRoleAsync(createdUser, role.Result.Name);
+                    if(!addToRoleResult.Succeeded)
+                    {
+                        AddIdentityErrors(addToRoleResult);
+                    }
                     return RedirectToAction("Index", "User");
                 }
                 else
                 {
-                    foreach (var error in createdUserResult.Errors)
-                    {
-                        ModelState.AddModelError(string.Empty, error.Description);
-                    }
+                    AddIdentityErrors(createdUserResult);
                     return View(user);
                 }
             }
@@ -100,54 +113,54 @@ namespace EShop.Areas.Admin.Controllers
                 existingUser.PhoneNumber = user.PhoneNumber;
                 existingUser.RoleId = user.RoleId;
 
-                var createdUserResult = await _userManager.CreateAsync(user, user.PasswordHash);
-                if (createdUserResult.Succeeded)
+                var updatedUserResult = await _userManager.UpdateAsync(existingUser);
+                if (updatedUserResult.Succeeded)
                 {
                     return RedirectToAction("Index", "User");
                 }
                 else
                 {
-                    foreach (var error in createdUserResult.Errors)
-                    {
-                        ModelState.AddModelError(string.Empty, error.Description);
-                    }
-                    return View(user);
+                    AddIdentityErrors(updatedUserResult);
+                    return View(existingUser);
                 }
             }
-            else
-            {
-                TempData["error"] = "Model có một vài thứ đang bị lỗi";
-                List<string> errors = new List<string>();
-                foreach (var value in ModelState.Values)
-                {
-                    foreach (var error in value.Errors)
-                    {
-                        errors.Add(error.ErrorMessage);
-                    }
-                }
-                string errorMas = string.Join("\n", errors);
-                return BadRequest(errorMas);
-            }
+            var roles = await _roleManager.Roles.ToListAsync();
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+
+            //Model validation failed
+            TempData["error"] = "Model có một vài thứ đang bị lỗi";
+            var errors = ModelState.Values.SelectMany(v => v.Errors.Select(e=>e.ErrorMessage)).ToList();
+            string errorMessage = string.Join("\n", errors);
+
+            return View(existingUser);
         }
 
         public async Task<IActionResult> Delete(string id)
         {
-            if(string.IsNullOrEmpty(id))
+            if (string.IsNullOrEmpty(id))
             {
                 return NotFound();
             }
             var user = await _userManager.FindByIdAsync(id);
-            if(user == null)
+            if (user == null)
             {
                 return NotFound();
             }
             var deletedResult = await _userManager.DeleteAsync(user);
-            if(!deletedResult.Succeeded)
+            if (!deletedResult.Succeeded)
             {
                 return View("Error");
             }
             TempData["Success"] = "Người dùng đã được xóa thành công!";
             return RedirectToAction("Index");
+        }
+
+        private void AddIdentityErrors(IdentityResult identityResult)
+        {
+            foreach (var error in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }

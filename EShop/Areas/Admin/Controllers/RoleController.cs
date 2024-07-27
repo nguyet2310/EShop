28c25272605e23778db726618385e9fb2667254c
nguyet2310 <phamnguyet2310@gmail.com>
2024-07-28 00:25:31 +0700

    List Roles

---
+++ EShop/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,22 @@
+﻿using EShop.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RoleController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public RoleController(DataContext dataContext)
+        {
+            _dataContext=dataContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Roles.OrderByDescending(r => r.Id).ToListAsync());
+        }
+    }
+}

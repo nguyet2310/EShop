ff226b709fbfc2a5c5b41f512607fb0f0452e425
nguyet2310 <phamnguyet2310@gmail.com>
2024-07-29 00:12:16 +0700

    Edit role

--- EShop/Areas/Admin/Controllers/RoleController.cs
+++ EShop/Areas/Admin/Controllers/RoleController.cs
@@ -41,6 +41,49 @@ namespace EShop.Areas.Admin.Controllers
             return RedirectToAction("Index");
         }
 
+        public async Task<IActionResult> Edit(string id)
+        {
+            if(string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+            var role = await _roleManager.FindByIdAsync(id);
+            return View(role);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, IdentityRole model)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid) //validate model state before proceeding
+            {
+                var role = await _roleManager.FindByIdAsync(id);
+                if(role == null)
+                {
+                    return NotFound();
+                }
+                role.Name = model.Name; //update role properties with model data
+                
+                try
+                {
+                    await _roleManager.UpdateAsync(role);
+                    TempData["success"] = "Role updated successfully!";
+                    return RedirectToAction("Index");
+                }
+                catch(Exception ex)
+                {
+                    ModelState.AddModelError("", "An error occurred while deleting the role.");
+                }
+            }
+            //If model is invalid or role not found, return the view with the model (or an empty model
+            //for a new role)
+            return View(model ?? new IdentityRole { Id = id}); //Pre-populate Id or provide an empty model
+        }
+
         public async Task<IActionResult> Delete(string id)
         {
             if(string.IsNullOrEmpty(id))

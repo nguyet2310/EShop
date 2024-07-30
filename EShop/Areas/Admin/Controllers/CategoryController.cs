9e5e516b9721e9f99493cc69f5d12170ccb3bb9f
nguyet2310 <phamnguyet2310@gmail.com>
2024-07-31 01:01:15 +0700

    Paginate category

--- EShop/Areas/Admin/Controllers/CategoryController.cs
+++ EShop/Areas/Admin/Controllers/CategoryController.cs
@@ -18,12 +18,39 @@ namespace EShop.Areas.Admin.Controllers
         {
             _dataContext=dataContext;
         }
-        public async Task<IActionResult> Index()
-        {
+		//public async Task<IActionResult> Index()
+		//{
 
-            return View(await _dataContext.Categories.OrderByDescending(p => p.Id).ToListAsync());
-        }
-        public IActionResult Create()
+		//    return View(await _dataContext.Categories.OrderByDescending(p => p.Id).ToListAsync());
+		//}
+
+		public async Task<IActionResult> Index(int pg = 1)
+		{
+			List<CategoryModel> category = _dataContext.Categories.ToList(); //33 datas
+
+
+			const int pageSize = 10; //10 items/trang
+
+			if (pg < 1) //page < 1;
+			{
+				pg = 1; //page ==1
+			}
+			int recsCount = category.Count(); //33 items;
+
+			var pager = new Paginate(recsCount, pg, pageSize);
+
+			int recSkip = (pg - 1) * pageSize; //(3 - 1) * 10; 
+
+			//category.Skip(20).Take(10).ToList()
+
+			var data = category.Skip(recSkip).Take(pager.PageSize).ToList();
+
+			ViewBag.Pager = pager;
+
+			return View(data);
+		}
+
+		public IActionResult Create()
         {
             return View();
         }
---
+++ EShop/Models/Paginate.cs
@@ -0,0 +1,48 @@
+﻿namespace EShop.Models
+{
+	public class Paginate
+	{
+        public int TotalItems { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int StartPage { get; set; }
+        public int EndPage { get; set; }
+
+        public Paginate() 
+        {
+            
+        }
+        public Paginate(int totalItems, int page, int pageSize=10) //10 items/trang
+        {
+            //làm tròn tổng items/10 items trên 1 trang. VD: 16 items/10 = tròn 3 trang
+            int totalPages = (int)Math.Ceiling((decimal)totalItems/(decimal)pageSize);
+
+            int currentPage = page; //page hiện tại = 1
+
+            int startPage = currentPage - 5; //trang bắt đầu trừ 5 button
+            int endPage = currentPage + 4; //trang cuối sẽ cộng thêm 4 button
+
+            if(startPage <= 0)
+            {
+                //nếu số trang nhỏ hơn hoặc bằng 0 thì số trang cuối sẽ bằng
+                endPage = endPage - (startPage - 1);
+                startPage = 1;
+            }
+			if (endPage > totalPages) //nếu số page cuối > số tổng trang 
+			{
+				endPage = totalPages; //số page cuối = số tổng trang
+				if (endPage > 10) //nếu số page cuối > 10
+				{
+					startPage = endPage - 9; //trang bắt đầu = trang cuối - 9
+				}
+			}
+			TotalItems = totalItems;
+			CurrentPage = currentPage;
+			PageSize = pageSize;
+			TotalPages = totalPages;
+			StartPage = startPage;
+			EndPage = endPage;
+		}
+	}
+}
